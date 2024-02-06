c8dfefe0e03dc73147805dcb9c2203593eb5b6c3
mattBava <XxpenguinxX123@gmail.com>
2024-02-06 11:31:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ let reviews =[
 
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "booknotes",
-    password: "Fu11St@ck",
-    port: 5432,
+     user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
   });
   db.connect();
 
